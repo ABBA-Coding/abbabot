70e8b008e4c246240a766f6bb906c426605bef90
Mukenzii <mcmuhi13@gmail.com>
2024-04-09 09:50:07 +0500

    1
    Changed

--- bazaapp/tests.py
+++ bazaapp/tests.py
@@ -1,5 +1,25 @@
 from django.test import TestCase
 
-from views import head_by_category
+from datetime import datetime
 
-head_by_category()
+
+def days_since_event(event_date):
+    """
+    Calculate the number of days since the event date.
+
+    Parameters:
+        event_date (str): The date of the event in the format 'YYYY-MM-DD'.
+
+    Returns:
+        int: Number of days since the event.
+    """
+    event_date = datetime.strptime(event_date, '%Y-%m-%d').date()
+    current_date = datetime.now().date()
+    delta = current_date - event_date
+    return delta.days
+
+
+# Example usage:
+event_date = '2024-04-08'  # Change this to the date of the event
+days_since = days_since_event(event_date)
+print("Days since the event:", days_since)
