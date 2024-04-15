1a6cd8ea0ad165db3d4197d5e7dacac0012e5658
abbauzb <productgsm@gmail.com>
2024-04-15 15:46:09 +0000

    fix celery running with redis

--- bazasite/celery.py
+++ bazasite/celery.py
@@ -1,6 +1,8 @@
+import os
+
 from celery.schedules import crontab
 from celery import Celery
-
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bazasite.settings")
 app = Celery('bazasite')
 app.config_from_object('django.conf.settings', namespace='CELERY')
 app.autodiscover_tasks()
